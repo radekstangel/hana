b8d543e69c339d0fd2c9b1bd049419210f96ad01
Radek Stangel <git@radekstangel.com>
2018-11-20 16:13:29 +0100

    Create basic app router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 // dependencies
 import GlobalStyles from "styles/global";
 import React from "react";
+import { Router } from "@reach/router";
+// pages
+import Ask from "pages/Ask";
+import Jobs from "pages/Jobs";
+import New from "pages/New";
+import Show from "pages/Show";
+import Top from "pages/Top";
 
 const App = () => {
   return (
-    <div>
+    <>
       <GlobalStyles />
-      <h1>It works!</h1>
-    </div>
+      <Router>
+        <Top path="/" />
+        <New path="new" />
+        <Show path="show" />
+        <Ask path="ask" />
+        <Jobs path="jobs" />
+      </Router>
+    </>
   );
 };
 
