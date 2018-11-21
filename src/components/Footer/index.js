631a27a6b15feafd7172dc37e92238c95ff09b47
Radek Stangel <git@radekstangel.com>
2018-11-21 16:17:00 +0100

    Add Footer componet

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+// dependencies
+import React from "react";
+
+// components
+import Box from "../box";
+import Wrapper from "./wrapper";
+
+const Footer = () => (
+  <Wrapper>
+    <Box>Developed in 2018 by Radek Stangel.</Box>
+  </Wrapper>
+);
+
+export default Footer;
