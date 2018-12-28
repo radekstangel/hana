65c650fb89bb4f710a0e2672232c352e6d0e2a71
Radek Stangel <git@radekstangel.com>
2018-12-28 14:37:21 +0100

    Add global styles

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,52 +1,30 @@
 // dependencies
 import { createGlobalStyle } from "styled-components";
-
-// styles settings
-import { color, font, media } from "./theme";
+// styles
+import { color, media } from "./settings";
 
 const GlobalStyles = createGlobalStyle`
   body {
-
-    ${media.small} {
-      font-size: ${18 / 16}rem;
-    }
-
-    ${media.medium} {
+    background: ${color.white};
+    color: ${color.blackLight};
+    font-size: ${18 / 16}rem;
+    /* ${media.large} {
       font-size: ${20 / 16}rem;
-    }
-
-    ${media.large} {
-      font-size: ${22 / 16}rem;
-    }
-  }
-
-  /* Typography _________________________ */
-
-  /* type reset */
-  h1, h2, h3, p, ul, ol, li {
-  margin: 0;
-  padding: 0;
-  }
-  /* vertical rhythm */
-  p, ul, ol {
-    margin-bottom: ${font.lineHeight}em;
+    } */
   }
 
   /* headings */
-  h1, h2, h3 {
+  h2 {
     color: ${color.black};
-    font-family: ${font.family.light};
-    font-weight: ${font.weight.light};
+    font-size: 2.369em;
     line-height: 1.2;
     margin-bottom: 0.5em;
   }
-  h1 { font-size: 3.157em; }
-  h2 { font-size: 2.369em; }
-  h3 { font-size: 1.777em; }
 
   /* link */
   a {
     color: ${color.primary};
+    font-size: 1rem;
     text-decoration: none;
 
     &:active, &:focus, &:hover {
@@ -55,19 +33,17 @@ const GlobalStyles = createGlobalStyle`
     }
   }
 
-  /* lists */
-  ul, ol {
-    list-style-position: inside;
-    margin-left: 1.5em;
-
-    & & {
-      margin-left: 1em;
-      margin-bottom: 0;
-    }
+  /* list */
+  ol {
+    color: ${color.gray};
+    font-size: 14px;
+    padding-left: 24px;
   }
 
-  /* text */
-  b, strong { font-weight: ${font.weight.bold}; }
+  li {
+    margin: 0;
+    padding: 0;
+  }
 `;
 
 export default GlobalStyles;
---
+++ src/styles/settings.js
@@ -0,0 +1,18 @@
+// theme settings
+
+// color palette
+export const color = {
+  primary: "#005598", // menu & links
+  primaryHover: "#1966a2", // visited links ...  mix(#fff, primary, 97%)
+  white: "#f7fafc", // content bg ... mix(#fff, primary, 97%)
+  black: "#1f262b", // headings ... mix(#222, primary, 92%)
+  blackLight: "#3f454b", // text ... mix(#444, primary, 92%)
+  gray: "#7d8489" // list numbers ... mix(#888, primary, 92%)
+};
+
+// media queries
+export const media = {
+  small: "@media (min-width: 34.0625em)", // 545px
+  // medium: "@media (min-width: 42.8125em)", // 685px
+  large: "@media (min-width: 52.3125em)" // 837px
+};
