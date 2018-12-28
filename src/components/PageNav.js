de510180408c8eaeef5bc7724129efdb2c7b2b92
Radek Stangel <git@radekstangel.com>
2018-12-28 14:37:59 +0100

    Add page navigation blueprint

---
+++ src/components/PageNav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PageNav = params => {
+  return (
+    <p>
+      <a href="#">previous</a>
+      <a href="#">next</a>
+    </p>
+  );
+};
+
+export default PageNav;
